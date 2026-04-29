5f86905805a1cd27d6e7bd4806112b522fef71d3
Ankur  Shrivastava <ankurshrivastava@Ankurs-Mac-mini.local>
2026-04-29 11:41:18 +0530

    adding read all endpoint

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
+from sqlalchemy import Select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db import engine, get_session
@@ -26,10 +27,20 @@ async def index():
 
 @app.post("/create-post", response_model=ReadPost)
 async def create_post(
-    post_in: CreatePost, session: Annotated[AsyncSession, Depends(get_session)]
+    post_in: CreatePost,
+    session: Annotated[AsyncSession, Depends(get_session)],
 ):
     post_obj = Post(**post_in.model_dump())
     session.add(post_obj)
     await session.commit()
     await session.refresh(post_obj)
     return post_obj
+
+
+@app.get("/posts")
+async def get_posts(
+    session: Annotated[AsyncSession, Depends(get_session)],
+) -> list[ReadPost]:
+    stmt = Select(Post)
+    all_posts = (await session.execute(stmt)).scalars().all()
+    return all_posts
